d1cf031daa568bf7af89f25475338544c190d159
melody <myeonghee.son@gmail.com>
2021-11-15 13:41:55 +0900

    [Add] 반복문

--- Python/grammers/views.py
+++ Python/grammers/views.py
@@ -9,9 +9,110 @@ from django.http.response import JsonResponse
 
 class GrammerView(View):
     def get(self, request):
-
+        '''
+        [ 값 교환 ]
+        =======================
+        a, b = 10, 20
+        a, b = b, a
+        print(a, b)
+        => 결과 : 20, 10
         
+        [ 출력 ] 
+        =======================
+        print("number : ", a, b, c)
+        => number : 1, 2, 3
+        
+        print(i, sep=', ')
+        => 1, 2, 3
+        
+        print(i, sep='')
+        => 123
+        
+        print(i, sep='\n')
+        =>
+        1
+        2
+        3
+        
+        print(i, end=' ')
+        => 결과 : 1 2 3 6 7 14 21 42
+        
+        =======================
+            
+        [ 반복문 ]
+        
+        @ For
+        =======================
+        1) i 증가
+            for i in range(10):
+                print(i)
+            
+            => 결과 : 0~9
+            
+        2) i 감소
+            for i in range(10, 0, -2):
+                print(i)
+            => 결과 : 10, 8, 6, 4, 2
+    
+        2) 특정 조건에 해당하면 다시 반복문 처음으로 돌아가게하기
+            for i in range(10):
+                if i%2 == 0:
+                    continue
+                print(i) # 홀수만 출력됨
+                i=i+1
+                
+                
+            3) 특정 상황에서 반복문 멈추게 하기
+            ** for ~, else~ 구문 : for 문에서 break가 걸리면, 실행되지 않는다. break가 걸리지 않을 때만 실행된다. 
+            
+                for i in range(1,11):
+                    print(i)
+                    if i>15:
+                        break
+                else:
+                    print(11)
 
+                    
+                    
+        @ While : 조건이 들어간다. i<=10이 참일 때, 반복문 중지
+        =======================
+        1) i 증가
+            i=1
+            while i<=10:
+                print(i)
+                i=i+1
+            
+        2) i 감소     
+            i=10
+            while i>=1:
+                print(i)
+                i=i-1
+            
+        3) 무한반복을 멈추게 하거나, 특정 상황에서 반복문 멈추게 하기
+            i=1
+            while True:
+                print(i)
+                if i == 4:
+                    break
+                i=i+1         
+            
+        4) 특정 조건에 해당하면 다시 반복문 처음으로 돌아가게하기
+            ** 주의 while문에서 적용할 때는 i의 증감 연산자가 continue보다 먼저 오지 않으면, 반복문이 계속 돌게된다. 
+            i = 1
+            while i<10:
+                i=i+1
+                if i % 2 == 0:
+                    continue
+                print(i)
+            
+            => 결과 : 3, 5, 7, 9
+            
+      
+        '''
+        
+        
+        
+        
         
         return JsonResponse({"MESSAGE": "Hello"}, status=200)
 
