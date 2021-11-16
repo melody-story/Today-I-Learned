9bdb4d12e27a9e059d67aa277a61580bd8e719fa
melody <myeonghee.son@gmail.com>
2021-11-16 21:36:30 +0900

    [Add] 선형배열알고리즘문제풀이

--- Python/grammers/urls.py
+++ Python/grammers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from grammers.views import GrammerView, ProgrammersExample2View, ProgrammersExample3View
+from grammers.views import GrammerView, ProgrammersExample2View
 
 urlpatterns = [
     path('grammers', GrammerView.as_view()),
+    path('programmers_example2', ProgrammersExample2View.as_view()),
 ]
--- Python/grammers/views.py
+++ Python/grammers/views.py
@@ -570,3 +570,132 @@ class GrammerView(View):
             
         '''
         return JsonResponse({"MESSAGE": "Hello"}, status=200)
+        
+    
+# 2. 선형 배열 알고리즘 풀이
+class ProgrammersExample2View(View):
+    def get(self, request):
+        '''(02) 정렬된 리스트에 원소 삽입
+        리스트 L 과 정수 x 가 인자로 주어질 때, 리스트 내의 올바른 위치에 x 를 삽입하여 그 결과 리스트를 반환하는 함수 solution 을 완성하세요.
+
+        인자로 주어지는 리스트 L 은 정수 원소들로 이루어져 있으며 크기에 따라 (오름차순으로) 정렬되어 있다고 가정합니다.
+
+        예를 들어, L = [20, 37, 58, 72, 91] 이고 x = 65 인 경우, 올바른 리턴 값은 [20, 37, 58, 65, 72, 91] 입니다.
+
+        힌트: 순환문을 이용하여 올바른 위치를 결정하고 insert() 메서드를 이용하여 삽입하는 것이 한 가지 방법입니다.
+
+        주의: 리스트 내에 존재하는 모든 원소들보다 작거나 모든 원소들보다 큰 정수가 주어지는 경우에 대해서도 올바르게 처리해야 합니다.
+        '''
+        x = 65
+    
+        L = [20, 37, 58, 72, 91]
+        
+        def solution1(L, x): # 풀이 실패 -> 시간 초과 및 반복문 멈추는 것 고려하지 않음
+            answer = []
+            for y in L:
+                if x > y:
+                    if L.index(y) != len(L)-1:
+                        L.insert(L.index(y)+1,x)
+                    else: L.append(x)
+                elif x < y:
+                    if L.index(y) == 0:
+                        L.insert(0,x)
+                    else:
+                        L.insert(L.index(y)-1,x)
+                        
+            answer = L        
+            return answer
+        
+        def solution2(L, x): # 풀이 실패 -> enumerate를 사용하였으나, 역시 시간 초과 및 반복문을 멈추는 것을 고려하지 않음.
+            answer = []
+            for index, value in enumerate(L) :
+                if x > value:
+                    if index != len(L)-1:
+                        L.insert(index+1,x)
+                    else: L.append(x)
+                elif x < value:
+                    if index == 0:
+                        L.insert(0,x)
+                    else:
+                        L.insert(index,x)
+            answer = L        
+            return answer
+        
+        def solution3(L, x): # PASS!!!
+            # 반복문이 계속 도는 것이 문제였으며, 이로인해 시간이 초과되는 것을 깨달음. 이를 잡아주니 풀이 성공!!!!! 
+            for index, value in enumerate(L) :
+                if x > value:
+                    if index == len(L)-1:
+                        L.append(x)
+                        break
+                    else:
+                        continue
+                else: 
+                    L.insert(index,x)
+                    break
+            return L 
+        
+        def solution4(L, x): # 나와 비슷한 풀이
+            for idx, num in enumerate(L):
+                if num > x:
+                    L.insert(idx,x)
+                    break
+
+                if L[-1] < x: # L[-1]은 리스트 맨 끝에 있는 수를 지칭 한다!!, 맨 끝자리이면서, x가 값이 크다는 것을 모두 충족함.
+                    L.append(x)
+                else:
+                    pass
+            return L
+        
+        def solution5(L, x): # 오름차순이라는 문제의 함정을 이용한 문제 풀이
+            L.append(x)
+            L.sort()
+            return L
+
+        return JsonResponse({"RESULT": solution3(L,x)}, status=200)
+
+    def post(self, request):
+        
+        '''
+        문제 설명
+        인자로 주어지는 리스트 L 내에서, 또한 인자로 주어지는 원소 x 가 발견되는 모든 인덱스를 구하여 
+        이 인덱스들로 이루어진 리스트를 반환하는 함수 solution 을 완성하세요.
+
+        리스트 L 은 정수들로 이루어져 있고 그 순서는 임의로 부여되어 있다고 가정하며, 동일한 원소가 반복하여 들어 있을 수 있습니다. 
+        이 안에 정수 x 가 존재하면 그것들을 모두 발견하여 해당 인덱스들을 리스트로 만들어 반환하고, 
+        만약 존재하지 않으면 하나의 원소로 이루어진 리스트 [-1] 를 반환하는 함수를 완성하세요.
+
+        예를 들어, L = [64, 72, 83, 72, 54] 이고 x = 72 인 경우의 올바른 리턴 값은 [1, 3] 입니다.
+        또 다른 예를 들어, L = [64, 72, 83, 72, 54] 이고 x = 83 인 경우의 올바른 리턴 값은 [2] 입니다.
+        마지막으로 또 다른 예를 들어, L = [64, 72, 83, 72, 54] 이고 x = 49 인 경우의 올바른 리턴 값은 [-1] 입니다.
+
+        힌트 1: 리스트의 index() 메서드와 리스트 슬라이싱을 활용하는 것이 한 가지 방법이 됩니다. 리스트 슬라이싱은 아래와 같이 동작합니다.
+
+        L = [6, 2, 8, 7, 3] 인 경우
+        L[1:3] = [2, 8]
+        L[2:] = [8, 7, 3]
+        L[:3] = [6, 2, 8]
+
+        힌트 2: 리스트의 index() 메서드는, 인자로 주어지는 원소가 리스트 내에 존재하지 않을 때 ValueError 를 일으킵니다. 
+        이것을 try ... except 로 처리해도 되고, "if x in L" 과 같은 조건문으로 특정 원소가 리스트 내에 존재하는지를 판단해도 됩니다.
+        '''
+        x= 72
+        L= [64, 72, 83, 72, 54]
+        def solution1(L,x): # 나의 풀이 ) PASS!!
+            answer =[]
+            validate=True
+            for indx, num in enumerate(L):
+                if x == num:
+                    answer.append(indx)
+                    validate=False
+            if validate:
+                answer=[-1]   
+            return answer
+        
+        def solution2(L, x): # 다른 풀이 : 리스트 컴프리헨션으로 깔끔하게!
+            if x in L:
+                return [i for i, y in enumerate(L) if y == x] # 조건문이 만족 할 떄, i를 리스트에 담는다.
+            else:
+                return [-1]
+        
+        return JsonResponse({"RESULT": solution1(L,x)}, status=200)
