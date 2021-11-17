0027fdabc8f3a206249397dcc424afb957f9239d
melody <myeonghee.son@gmail.com>
2021-11-17 18:53:21 +0900

    [Add] 연결리스트 순회 알고리즘문제풀이

--- Python/grammers/views.py
+++ Python/grammers/views.py
@@ -570,8 +570,6 @@ class GrammerView(View):
             
         '''
         return JsonResponse({"MESSAGE": "Hello"}, status=200)
-        
-    
 # 2. 선형 배열 알고리즘 풀이
 class ProgrammersExample2View(View):
     def get(self, request):
@@ -926,3 +924,52 @@ class ProgrammersExample5View(View):
                 return "solution2(L, x, mid+1, u)"
         
         return  JsonResponse({"RESULT": solution2(L, x, l, u)}, status=200)
+
+class ProgrammersExample7View(View):
+    '''
+    (07) 연결 리스트 순회
+    문제 설명
+    제 7 강에서 소개된 추상적 자료구조로 LinkedList 라는 이름의 클래스가 정의되어 있다고 가정하고, 이 리스트를 처음부터 끝까지 순회하는 메서드 traverse() 를 완성하세요.
+
+    메서드 traverse() 는 리스트를 리턴하되, 이 리스트에는 연결 리스트의 노드들에 들어 있는 데이터 아이템들을 연결 리스트에서의 순서와 같도록 포함합니다. 예를 들어, LinkedList L 에 들어 있는 노드들이 43 -> 85 -> 62 라면, 올바른 리턴 값은 [43, 85, 62] 입니다.
+
+    이 규칙을 적용하면, 빈 연결 리스트에 대한 순회 결과로 traverse() 메서드가 리턴해야 할 올바른 결과는 [] 입니다.
+
+    [참고] "실행" 을 눌렀을 때 통과하는 것은 아무 의미 없습니다. 
+    '''
+    def get (self, request):
+        class Node:
+            def __init__(self, item):
+                self.data = item
+                self.next = None
+
+        class LinkedList:
+            def __init__(self):
+                self.nodeCount = 0
+                self.head = None
+                self.tail = None
+
+            def getAt(self, pos):
+                if pos < 1 or pos > self.nodeCount:
+                    return None
+                i = 1
+                curr = self.head
+                while i < pos:
+                    curr = curr.next
+                    i += 1
+                return curr
+
+            def traverse(self):
+                answer=[]
+                a = self.head
+                while a != None:            # 빈 리스트라면 head와 tail이 None
+                    answer.append(a.data) 
+                                            # 빈 리스트가 아니라면 헤더가 존재하므로 추가, 
+                                            # a는 노드의 순서만 의미하므로, 노드이 데이터에 접근하려면 a.data를 해주어야함. 
+                    a = a.next              # 헤드의 다음 연결 노드로 접근
+                return answer
+
+            def solution(x):
+                return 0
+        
+        return  JsonResponse({"RESULT": []}, status=200)
