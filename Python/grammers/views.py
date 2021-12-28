08325cd4b9e5e53d1d9757a683f6a7a5fe2004ef
melody <myeonghee.son@gmail.com>
2021-12-28 17:54:47 +0900

    알고리즘 풀이(18-02.이진트리의 전위순회 연산 구현)

--- Python/grammers/views.py
+++ Python/grammers/views.py
@@ -2554,3 +2554,74 @@ class ProgrammersExample16View(View):
         '''        
 def solution(x):
     return 0
+
+
+'''
+(18-02) 이진트리의 전위순회 연산 구현
+문제 설명
+이미 주어진 코드 (class Node 와 class BinaryTree 에 의하여) 의 구조를 따르는 이진 트리 (binary tree) 에 대하여, 트리를 전위 순회 (preorder traversal) 하는 연산의 구현을 완성하세요.
+
+초기 코드에 pass 로만 되어 있는 class Node 의 preorder() 메서드와 class BinaryTree 의 preorder() 메서드를 구현합니다. 코드의 다른 부분은 수정할 필요가 없습니다.
+
+참고로 할 수 있도록, 강의에서 소개한 inorder() 메서드들 (class Node 와 class BinaryTree 에 대해서) 을 그대로 두었습니다. 문제로 주어진 preorder() 연산도 매우 비슷한 식으로 구현할 수 있으니 참고로 삼으세요.
+
+[참고] 실행 을 눌렀을 때 통과하는 것은 아무 의미 없습니다.
+또한, solution() 함수는 테스트에 영향을 미치므로 수정하지 말고 그대로 두세요.
+'''
+class Node:
+    
+    def __init__(self, item):
+        self.data = item
+        self.left = None
+        self.right = None
+
+
+    def inorder(self):
+        traversal = []
+        if self.left:
+            traversal += self.left.inorder()
+        traversal.append(self.data)
+        if self.right:
+            traversal += self.right.inorder()
+        return traversal
+
+    # 나의 풀이
+    def preorder(self):
+        traversal = []
+        traversal.append(self.data)
+        if self.left:
+            traversal += self.left.preorder()
+        if self.right:
+            traversal += self.right.preorder()
+        return traversal
+            
+
+
+class BinaryTree:
+
+    def __init__(self, r):
+        self.root = r
+
+
+    def inorder(self):
+        if self.root:
+            return self.root.inorder()
+        else:
+            return []
+
+    #나의 풀이
+    def preorder(self):
+        if self.root:
+            return self.root.preorder()
+        else:
+            return [] 
+    '''
+    테스트 1 〉	통과 (0.03ms, 16.6MB)
+    테스트 2 〉	통과 (0.04ms, 16.6MB)
+    테스트 3 〉	통과 (0.04ms, 16.5MB)
+    테스트 4 〉	통과 (0.04ms, 16.5MB)
+    테스트 5 〉	통과 (0.04ms, 16.5MB)
+    '''
+
+def solution(x):
+    return 0
