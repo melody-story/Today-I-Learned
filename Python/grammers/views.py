4ac03772a7aaecd329fd960bc710b8ece749ff4b
melody <myeonghee.son@gmail.com>
2021-12-28 17:57:54 +0900

    알고리즘 풀이(21.이진 탐색 트리에서 노드의 삭제 연산 구현)

--- Python/grammers/views.py
+++ Python/grammers/views.py
@@ -2841,3 +2841,212 @@ class BinSearchTree:
 
 def solution(x):
     return 0
+
+
+
+'''
+(21) 이진 탐색 트리에서 노드의 삭제 연산 구현
+문제 설명
+초기 코드에 주어진 class Node 와 class BinSearchTree 를 기반으로, 이진 탐색 트리 (binary search tree) 에서 지정된 원소를 삭제하는 remove(key) 연산의 구현을 완성하세요.
+
+class Node 와 class BinSearchTree 에 이미 구현되어 있는 코드는 수정하지 마세요. 코드 구현의 정확성 평가에 이용됩니다. 초기 코드에 들어 있는 주석을 참고로 하여, BinSearchTree::remove() 메서드의 안에 들어 있는 pass 를 없애고 그 자리에 올바른 코드를 써 넣으면 됩니다.
+
+[참고 1] solution() 함수의 구현은 그대로 두세요. 이것을 없애면 테스트가 되지 않습니다.
+
+[참고 2] "코드 실행" 을 눌렀을 때 통과하는 것은 아무런 의미가 없습니다.
+
+[참고 3] 잘 생각해 보면, 이진 탐색 트리를 구현하지 않고 키 순서대로 정렬된 Python 의 배열을 유지함으로써도 같은 연산을 구현할 수 있습니다. 
+        이 연습문제에서는 효율성 테스트를 하지 않기 때문에 이러한 구현을 오답으로 간주하지 않습니다만, 배열을 이용한 구현과 트리 구조를 이용한 구현은 연산의 복잡도에 큰 차이가 있습니다. 
+        이진 탐색 트리로 구현하여 코드 작성 연습을 하시기 바랍니다.
+'''
+
+class Node:
+    
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+    def insert(self, key, data):
+        if key < self.key:
+            if self.left:
+                self.left.insert(key, data)
+            else:
+                self.left = Node(key, data)
+        elif key > self.key:
+            if self.right:
+                self.right.insert(key, data)
+            else:
+                self.right = Node(key, data)
+        else:
+            raise KeyError('Key %s already exists.' % key)
+
+
+    def lookup(self, key, parent=None):
+        if key < self.key:
+            if self.left:
+                return self.left.lookup(key, self)
+            else:
+                return None, None
+        elif key > self.key:
+            if self.right:
+                return self.right.lookup(key, self)
+            else:
+                return None, None
+        else:
+            return self, parent
+
+
+    def inorder(self):
+        traversal = []
+        if self.left:
+            traversal += self.left.inorder()
+        traversal.append(self)
+        if self.right:
+            traversal += self.right.inorder()
+        return traversal
+
+
+    def countChildren(self):
+        count = 0
+        if self.left:
+            count += 1
+        if self.right:
+            count += 1
+        return count
+
+
+class BinSearchTree:
+
+    def __init__(self):
+        self.root = None
+
+
+    def insert(self, key, data):
+        if self.root:
+            self.root.insert(key, data)
+        else:
+            self.root = Node(key, data)
+
+
+    def lookup(self, key):
+        if self.root:
+            return self.root.lookup(key)
+        else:
+            return None, None
+
+
+    def remove(self, key):
+        node, parent = self.lookup(key)
+        if node:
+            nChildren = node.countChildren()
+            # The simplest case of no children
+            if nChildren == 0:
+                # 만약 parent 가 있으면
+                if parent:
+                    if parent.left.key == node.key:
+                        parent.left = None
+                    else:
+                        parent.right = None
+                # node 가 왼쪽 자식인지 오른쪽 자식인지 판단하여
+                # parent.left 또는 parent.right 를 None 으로 하여
+                # leaf node 였던 자식을 트리에서 끊어내어 없앱니다.
+                
+                # 만약 parent 가 없으면 (node 는 root 인 경우)
+                # self.root 를 None 으로 하여 빈 트리로 만듭니다.
+                else:
+                    self.root = None
+            # When the node has only one child
+            elif nChildren == 1:
+                # 하나 있는 자식이 왼쪽인지 오른쪽인지를 판단하여
+                # 그 자식을 어떤 변수가 가리키도록 합니다.
+                if node.left:
+                    s = node.left
+                else:
+                    s = node.right
+                # 만약 parent 가 있으면
+                # node 가 왼쪽 자식인지 오른쪽 자식인지 판단하여
+                # 위에서 가리킨 자식을 대신 node 의 자리에 넣습니다.
+                if parent:
+                    if parent.left.key == node.key:
+                        parent.left = s
+                        s = None
+                    else:
+                        parent.right = s
+                        s = None
+                # 만약 parent 가 없으면 (node 는 root 인 경우)
+                # self.root 에 위에서 가리킨 자식을 대신 넣습니다.
+                else:
+                    self.root = s
+            # When the node has both left and right children
+            else:
+                # 1차 풀이(오답)
+                parent = node.right
+                s = parent.left
+                while s.left:
+                    parent = s
+                    s = s.left
+                node.key = s.key
+                node.data = s.data
+                if s.right:
+                    parent.left = s.right
+                if s.countChilde() == 0:
+                    parent.left = None
+                    
+                '''
+                테스트 1 〉	통과 (0.07ms, 16.8MB)
+                테스트 2 〉	통과 (0.04ms, 16.7MB)
+                테스트 3 〉	통과 (0.07ms, 16.7MB)
+                테스트 4 〉	통과 (0.06ms, 16.7MB)
+                테스트 5 〉	실패 (런타임 에러)
+                테스트 6 〉	실패 (런타임 에러)
+                테스트 7 〉	통과 (0.06ms, 16.9MB)
+                테스트 8 〉	실패 (런타임 에러)
+                테스트 9 〉	실패 (런타임 에러)
+                '''
+                    
+                    
+                # 2차 풀이(정답)
+                s = node.right
+                parent = node
+                while s.left:
+                    parent = s
+                    s = parent.left
+                    
+                node.key = s.key
+                node.data = s.data
+                
+                if parent.right == s: # node 가 곧 parent인 경우, s의 child가 left 가 없는 경우  
+                    parent.right = s.right                   
+                if parent.left == s:
+                    parent.left = s.right
+                    
+                '''
+                테스트 1 〉	통과 (0.13ms, 16.8MB)
+                테스트 2 〉	통과 (0.04ms, 16.7MB)
+                테스트 3 〉	통과 (0.07ms, 16.7MB)
+                테스트 4 〉	통과 (0.06ms, 16.8MB)
+                테스트 5 〉	통과 (0.06ms, 16.8MB)
+                테스트 6 〉	통과 (0.06ms, 16.7MB)
+                테스트 7 〉	통과 (0.06ms, 16.8MB)
+                테스트 8 〉	통과 (0.06ms, 16.7MB)
+                테스트 9 〉	통과 (0.07ms, 16.7MB)
+                '''
+
+            return True
+
+        else:
+            return False
+
+
+    def inorder(self):
+        if self.root:
+            return self.root.inorder()
+        else:
+            return []
+
+
+def solution(x):
+    return 0
