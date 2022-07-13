f8750ba134ffa0af96c03a110c30c73b36311ee4
myeonghee Son <melody@myeonghees-MacBook-Pro.local>
2022-07-14 03:52:03 +0900

    ADD: 1.1 - 2.2

---
+++ momentum/app.js
@@ -0,0 +1,14 @@
+
+// ```
+// const 는 상수 즉 변하지 않는 수  값을 업데이트할 수 없음
+// let 아래에서 변수의 값을 업데이트 할 수 있다.
+// 변수 convetion : SnakeCase
+// ```
+alert("Melody");
+const a = 5; 
+const b = 2; 
+const myName = "Melody"; 
+console.log(a * b)
+console.log(a - b)
+console.log(a + b)
+console.log("hello"+myName)
