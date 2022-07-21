7371d2a564f9cbe61b0bf27f965496533e4b809e
myeonghee Son <melody@myeonghees-MacBook-Pro.local>
2022-07-21 16:18:25 +0900

    ADD: 3.9 - 4.5

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,19 @@
-const h1 = document.querySelector(".hello:first-child h1");
+const loginInput =  document.querySelector("#login-form input");
+const loginForm =  document.querySelector("#login-form");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"
 
-function handelTitleClick() {
-    h1.classList.toggle("clicked");
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    const username = loginInput.value;
+    console.log(username);
+    console.dir(greeting)
+    localStorage.setItem("username", username)
+    // greeting.innerText = "Hi " + username
+    greeting.innerText = `Hi ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-h1.addEventListener("click", handelTitleClick);
+loginForm.addEventListener("submit", onLoginSubmit);
