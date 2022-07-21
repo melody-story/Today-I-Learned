f1ed6278cc36be5f8e2c8b856b6492540e198986
myeonghee Son <melody@myeonghees-MacBook-Pro.local>
2022-07-21 16:36:12 +0900

    ADD: 4.6 - 4.7

--- momentum/app.js
+++ momentum/app.js
@@ -3,17 +3,29 @@ const loginForm =  document.querySelector("#login-form");
 const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME)
     const username = loginInput.value;
-    console.log(username);
-    console.dir(greeting)
-    localStorage.setItem("username", username)
-    // greeting.innerText = "Hi " + username
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreeting(username)
+}
+function paintGreeting(username) {
     greeting.innerText = `Hi ${username}`
     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+console.log(savedUsername);
+
+if (savedUsername === null) {
+    // savedUsername이 null이면 form 보이게
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener("submit", onLoginSubmit);
+    
+} else {
+    // savedUsername이 있으면, h1이 나타나도록!
+    paintGreeting(savedUsername)  
+}
